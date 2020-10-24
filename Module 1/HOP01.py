7aca34ca0a6a7b885fc13fa66eda9927ed12b2c0
AdamShandi <adam.cheleby@gmail.com>
2020-10-23 22:41:25 -0700

    Submission for Module 1

---
+++ Module 1/HOP01.py
@@ -0,0 +1,9 @@
+name= input('Please enter your name: ')
+guess= int(input('Please guess an integer between 1 adn 6: '))
+random_numebr=5
+
+
+if(guess == random_numebr):
+    print('COngrats, you got it!')
+else:
+    print('Oops, goodluck next time!')
